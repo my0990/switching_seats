128da97e9e2e993ef0421319c5b7b2afba448305
my0990 <my0990@naver.com>
2022-04-26 15:14:30 +0900

    test commit

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import logo from './logo.svg';
 import './App.css';
-import ReactFullpage from '@fullpage/react-fullpage';
+// import ReactFullpage from '@fullpage/react-fullpage';
 
-const Fullpage = () => (
-  <ReactFullpage
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
+// const Fullpage = () => (
+//   <ReactFullpage
+//     //fullpage options
+//     licenseKey = {'YOUR_KEY_HERE'}
+//     scrollingSpeed = {1000} /* Options here */
 
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1</p>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-          <div className='section'>
-            <p>Section 3</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
+//     render={({ state, fullpageApi }) => {
+//       return (
+//         <ReactFullpage.Wrapper>
+//           <div className="section">
+//             <p>Section 1</p>
+//           </div>
+//           <div className="section">
+//             <p>Section 2</p>
+//           </div>
+//           <div className='section'>
+//             <p>Section 3</p>
+//           </div>
+//         </ReactFullpage.Wrapper>
+//       );
+//     }}
+//   />
+// );
 
 function App() {
   return (
     <div className="App">
-      <Fullpage />
+      test
     </div>
   );
 }
